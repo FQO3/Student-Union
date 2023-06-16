4a2281b3d1e0f7a77ecbe08fb587efcb45d19c3d
FQO3 <111474347+FQO3@users.noreply.github.com>
2023-06-16 17:05:42 +0800

    add all to gayhub
    add all

---
+++ Student-Union/www.js
@@ -0,0 +1,48 @@
+﻿const express = require('express')
+const app = express();
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+
+app.use(function (req, res, next) {
+	if (!checkPC(req)) {
+		if (req.cookies.vis != '1') {
+			res.cookie('vis','1');
+			res.redirect(302, '/notpc.html');
+		}
+	}
+	var date = new Date();
+	var year = date.getFullYear();
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var second = date.getSeconds();
+	var Msecond = date.getMilliseconds();
+	console.log(year + 'y ' + month + 'm ' + day + 'd ' + hour + 'h ' + minute + 'm ' + second + 's ' + Msecond + '’', '  ', req.headers['x-forwarded-for'], '  ', req.ip, req.url, '  ', req.headers['sec-ch-ua-platform'], '  ');
+	next();
+});
+app.get('/notpc.html', function (req, res) {
+	res.sendFile(__dirname + "/client/" + "notpc.html");
+})
+// 监听 / 路径的 get 请求
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + "/client/" + "index.html");
+})
+
+app.use(express.static('client/'));
+
+const server = app.listen(81, function () {
+	console.log("服务器已启动, 监听81端口,http://127.0.0.1:81");
+})
+function checkPC(req){
+    // var agentstr = navigator.userAgent.toLowerCase();
+    var agentstr = req.headers['user-agent'].toLowerCase();  // nodejs
+	var agentreg = /(iphone|ipod|ipad|android|symbianos|windows phone|playbook|mobile)/;
+	var agentph = agentstr.match(agentreg);
+	if(agentph){
+		return false;
+	}else{
+		return true;
+	}
+}
